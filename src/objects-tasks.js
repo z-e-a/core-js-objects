6edea9848d9f2f72d958c235e71211043c369af1
z-e-a <e.a.zubkov@yandex.ru>
2024-03-02 15:45:07 +0300

    feat: solve task 'sellTickets'

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -151,8 +151,14 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cash = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      cash += 25;
+    } else if (queue[i] - 25 > cash) return false;
+  }
+  return true;
 }
 
 /**
